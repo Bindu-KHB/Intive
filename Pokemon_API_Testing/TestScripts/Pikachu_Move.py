772403b9dcb7b794cc789d692ff3704e835d46ba
Masthan Devalla <bindu.kethireddy@yahoo.com>
2021-03-17 22:51:28 +0100

    Update Pikachu_Move.py

--- Pokemon_API_Testing/TestScripts/Pikachu_Move.py
+++ Pokemon_API_Testing/TestScripts/Pikachu_Move.py
@@ -3,7 +3,6 @@ import json
 
 response = requests.get('https://pokeapi.co/api/v2/pokemon/pikachu')
 
-
 json_content = json.loads(response.content)
 
 for moves in json_content['moves']:
@@ -11,3 +10,17 @@ for moves in json_content['moves']:
     if pikachu_move == "thunder-punch":
         print("Verified thunder-punch move found")
 
+
+location_area_encounters_var = json_content['location_area_encounters']
+
+encounters_response = requests.get(location_area_encounters_var)
+encounters_content = json.loads(encounters_response.content)
+
+for encounter in encounters_content:
+    version_details = (encounter['version_details'][0])
+    name = (version_details['version']['name'])
+
+    if name == "yellow":
+        content = version_details['encounter_details'][0]
+        if(content['method']['name']) == 'gift':
+            print("Pikachu can be received as a gift")
